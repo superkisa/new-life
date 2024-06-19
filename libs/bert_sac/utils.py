296db78c8ceccddc29f20fd15926a949261c0018
superkisa <81680387+superkisa@users.noreply.github.com>
2024-06-19 13:14:16 +0300

    added env recorder util func

--- libs/bert_sac/utils.py
+++ libs/bert_sac/utils.py
@@ -1,5 +1,9 @@
 import gymnasium as gym
+import torch
 from gymnasium.envs.registration import EnvSpec
+from tqdm import tqdm
+
+from bert_sac.sac_trainer import AntSAC
 
 
 def make_env(env_id: str | EnvSpec, seed: int | None = None):
@@ -10,3 +14,28 @@ def make_env(env_id: str | EnvSpec, seed: int | None = None):
         return env
 
     return thunk
+
+
+def record_inference(
+    model: AntSAC,
+    env_name: str,
+    num_steps: int = 5_000,
+    episode_trigger=lambda t: t % 1 == 0,
+    save_folder="artifacts/recordings",
+) -> gym.Env:
+    obs_env = gym.make(env_name, render_mode="rgb_array")
+    num_actions = obs_env.action_space.shape[0]  # type: ignore
+    obs_env = gym.wrappers.RecordVideo(
+        obs_env, video_folder=save_folder, episode_trigger=episode_trigger
+    )
+
+    obs, _ = obs_env.reset()
+    for _ in tqdm(range(num_steps)):
+        obs = torch.Tensor(obs).to(model.device)
+        actions, _, _ = model.actor.get_action0(obs)
+        obs, _, terminated, truncated, _ = obs_env.step(
+            actions.view(num_actions).detach().cpu().numpy()
+        )
+        if terminated or truncated:
+            obs, _ = obs_env.reset()
+    return obs_env
